62263b3bdac4e63882c8bbca60121a6a148fc62f
corinamirica <corina_mirica@yahoo.com>
2025-08-14 10:36:13 +0300

    tema_cepitol3

---
+++ tema_2/cap3.py
@@ -0,0 +1,9 @@
+nota=int(input("Care este nota ta?"))
+if nota > 10:
+    print('Adevarul te va elibera!')
+
+elif nota >= 5:
+    print('Felicitari, ai promovat!')
+    
+else:
+    print('Mai invata!')
---
+++ tema_2/ex1_cap3.py
@@ -0,0 +1,19 @@
+varsta = int(input('Care este varsta ta?'))
+
+if varsta >=18:
+    print('Wow! Esti major!')
+
+else:
+     print('mai mananca mamaliga!')
+
+
+luna=int(input('In a cata luna e ziua ta?'))
+
+if luna>=3 and luna <=5:
+     print('Wow! Esti nascut primvara!')
+elif luna>=6 and luna <=8:
+    print('Wow! Esti nascut vara!')
+elif luna >=9 and luna <=11:
+     print('WOW! Ziua ta e toamna!')
+else:
+     print('WOW! Ziua ta e iarna!')
